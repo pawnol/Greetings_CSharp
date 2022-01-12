13f78311ea6a1880168e108714b6861330d3612b
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2022-01-12 10:35:38 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace Greetings
+{
+    partial class GreetingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.displayButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timesTextBox = new System.Windows.Forms.TextBox();
+            this.greetingsLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // displayButton
+            // 
+            this.displayButton.AutoSize = true;
+            this.displayButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayButton.Location = new System.Drawing.Point(128, 63);
+            this.displayButton.Name = "displayButton";
+            this.displayButton.Size = new System.Drawing.Size(172, 43);
+            this.displayButton.TabIndex = 0;
+            this.displayButton.Text = "Display Greetings";
+            this.displayButton.UseVisualStyleBackColor = true;
+            this.displayButton.Click += new System.EventHandler(this.displayButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(360, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "How many times do you want to see the greeting?";
+            // 
+            // timesTextBox
+            // 
+            this.timesTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timesTextBox.Location = new System.Drawing.Point(380, 27);
+            this.timesTextBox.Name = "timesTextBox";
+            this.timesTextBox.Size = new System.Drawing.Size(35, 26);
+            this.timesTextBox.TabIndex = 2;
+            // 
+            // greetingsLabel
+            // 
+            this.greetingsLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.greetingsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.greetingsLabel.Location = new System.Drawing.Point(18, 116);
+            this.greetingsLabel.Name = "greetingsLabel";
+            this.greetingsLabel.Size = new System.Drawing.Size(397, 172);
+            this.greetingsLabel.TabIndex = 3;
+            // 
+            // GreetingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 298);
+            this.Controls.Add(this.greetingsLabel);
+            this.Controls.Add(this.timesTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.displayButton);
+            this.Name = "GreetingsForm";
+            this.Text = "Greetings";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button displayButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox timesTextBox;
+        private System.Windows.Forms.Label greetingsLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Greetings
+ * Pawelski
+ * 1/11/2022
+ * Read the code and run the program a few times to see what it does. 
+ * Once you are familiar with the program, answer the questions found 
+ * on the activity sheet.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Greetings
+{
+    public partial class GreetingsForm : Form
+    {
+        public GreetingsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void displayButton_Click(object sender, EventArgs e)
+        {
+            const string GREETING = "Hello, there...";
+            int times;
+            string greetings = "";
+
+            times = Convert.ToInt32(timesTextBox.Text);
+            for (int i = 0; i < times; i++)
+            {
+                greetings += GREETING + "\n";
+            }
+            greetingsLabel.Text = greetings;
+        }
+    }
+}
